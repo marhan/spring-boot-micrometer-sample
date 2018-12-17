c9f9d87ba4384bfca615b62658e0474b7e7e177f
Markus Hanses <me@markushanses.de>
2018-12-17 15:13:28 +0100

    Content of home moved to its component.

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -4,13 +4,13 @@
 
             <b-navbar-toggle target="nav_collapse"></b-navbar-toggle>
 
-            <b-navbar-brand href="#">NavBar</b-navbar-brand>
+            <b-navbar-brand :to="{ path: '/' }">Micrometer Sample</b-navbar-brand>
 
             <b-collapse is-nav id="nav_collapse">
 
                 <b-navbar-nav>
-                    <b-nav-item href="#">Link</b-nav-item>
-                    <b-nav-item href="#" disabled>Disabled</b-nav-item>
+                    <b-nav-item :to="{ path: '/' }">Home</b-nav-item>
+                    <b-nav-item :to="{ path: 'business-cases' }">Business cases</b-nav-item>
                 </b-navbar-nav>
 
                 <!-- Right aligned nav items -->
@@ -40,69 +40,10 @@
 
             </b-collapse>
         </b-navbar>
-
         <!-- navbar-1.vue -->
-        <div class="container">
-            <div style="margin-top: 2rem; padding-bottom: 1rem;"><h1>Micrometer Sample</h1></div>
-            <!-- Alerts -->
-            <h2>Alerts</h2>
-            <div style="padding: 1rem;">
-                <div v-for="brand in ['success', 'info', 'warning', 'danger']" :key="brand" class="alert"
-                     :class="'alert-' + brand">
-                    <strong>Well done!</strong> You successfully read this {{brand}} alert message.
-                </div>
-            </div>
-            <h2>Breadcrumb</h2>
-            <div style="padding: 1rem;">
-                <ol class="breadcrumb">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item"><a href="#">Library</a></li>
-                    <li class="breadcrumb-item active">Data</li>
-                </ol>
-            </div>
-            <!-- Buttons-->
-            <h2>Buttons</h2>
-            <div style="padding: 1rem;">
-                <div v-for="type in ['', 'outline']" :key="type">
-				  <span v-for="brand in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']" :key="brand">
-					  <button role="button" class="btn" :class="'btn-' + (type.length ? (type + '-') : '') + brand"
-                              style="margin-bottom: 1rem; margin-right: 1rem;">
-						  {{brand}} {{type}}
-					  </button>
-				  </span>
-                </div>
-            </div>
-            <!-- Progress-->
-            <h2>Progress</h2>
-            <div style="padding: 1rem;">
-                <div v-for="brand in ['success', 'info', 'warning', 'danger']" :key="brand" class="progress"
-                     style='margin-bottom: 1rem;'>
-                    <div class="progress-bar" :class="'bg-' + brand" style="width: 25%" aria-valuenow="25"
-                         aria-valuemin="0" aria-valuemax="100"></div>
-                </div>
-            </div>
-            <!-- Notice-->
-            <h2>Modal</h2>
-            <div style="padding: 1rem;">
-                <div class="modal-dialog" role="document">
-                    <div class="modal-content">
-                        <div class="modal-header">
-                            <h5 class="modal-title">Modal title</h5>
-                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                <span aria-hidden="true">&times;</span>
-                            </button>
-                        </div>
-                        <div class="modal-body">
-                            <p>Modal body text goes here.</p>
-                        </div>
-                        <div class="modal-footer">
-                            <button type="button" class="btn btn-primary">Save changes</button>
-                            <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+
+        <router-view/>
+
     </div>
 </template>
 
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,24 +1,64 @@
 <template>
-    <b-jumbotron>
-        <template slot="header">
-            Bootstrap Vue
-        </template>
-        <template slot="lead">
-            This is a simple hero unit, a simple jumbotron-style component for
-            calling extra attention to featured content or information.
-        </template>
-        <hr class="my-4">
-        <p>
-            It uses utility classes for typography and spacing to space content
-            out within the larger container.
-        </p>
-        <b-btn variant="primary" href="#">Do Something</b-btn>
-        <b-btn variant="success" href="#">Do Something Else</b-btn>
-    </b-jumbotron>
+    <div class="container">
+        <div style="margin-top: 2rem; padding-bottom: 1rem;"><h1>Micrometer Sample</h1></div>
+        <!-- Alerts -->
+        <h2>Alerts</h2>
+        <div style="padding: 1rem;">
+            <div v-for="brand in ['success', 'info', 'warning', 'danger']" :key="brand" class="alert"
+                 :class="'alert-' + brand">
+                <strong>Well done!</strong> You successfully read this {{brand}} alert message.
+            </div>
+        </div>
+        <h2>Breadcrumb</h2>
+        <div style="padding: 1rem;">
+            <ol class="breadcrumb">
+                <li class="breadcrumb-item"><a href="#">Home</a></li>
+                <li class="breadcrumb-item"><a href="#">Library</a></li>
+                <li class="breadcrumb-item active">Data</li>
+            </ol>
+        </div>
+        <!-- Buttons-->
+        <h2>Buttons</h2>
+        <div style="padding: 1rem;">
+            <div v-for="type in ['', 'outline']" :key="type">
+				  <span v-for="brand in ['primary', 'secondary', 'success', 'info', 'warning', 'danger']" :key="brand">
+					  <button role="button" class="btn" :class="'btn-' + (type.length ? (type + '-') : '') + brand"
+                              style="margin-bottom: 1rem; margin-right: 1rem;">
+						  {{brand}} {{type}}
+					  </button>
+				  </span>
+            </div>
+        </div>
+        <!-- Progress-->
+        <h2>Progress</h2>
+        <div style="padding: 1rem;">
+            <div v-for="brand in ['success', 'info', 'warning', 'danger']" :key="brand" class="progress"
+                 style='margin-bottom: 1rem;'>
+                <div class="progress-bar" :class="'bg-' + brand" style="width: 25%" aria-valuenow="25"
+                     aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+        </div>
+        <!-- Notice-->
+        <h2>Modal</h2>
+        <div style="padding: 1rem;">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title">Modal title</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p>Modal body text goes here.</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-primary">Save changes</button>
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
-<script>
-    export default {
-        props: {},
-    };
-</script>
