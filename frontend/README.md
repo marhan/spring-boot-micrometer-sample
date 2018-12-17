151b213e6ab96861dd5af0889b2a2df7e50db228
Markus Hanses <me@markushanses.de>
2018-12-17 15:23:56 +0100

    Navigation improved.

--- frontend/README.md
+++ frontend/README.md
@@ -37,3 +37,7 @@ npm run test:unit
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+## Credits
+
+* [Bootstrap Vue.js](https://bootstrap-vue.js.org/)
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -16,26 +16,20 @@
                 <!-- Right aligned nav items -->
                 <b-navbar-nav class="ml-auto">
 
-                    <b-nav-form>
-                        <b-form-input size="sm" class="mr-sm-2" type="text" placeholder="Search"/>
-                        <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
-                    </b-nav-form>
-
-                    <b-nav-item-dropdown text="Lang" right>
-                        <b-dropdown-item href="#">EN</b-dropdown-item>
-                        <b-dropdown-item href="#">ES</b-dropdown-item>
-                        <b-dropdown-item href="#">RU</b-dropdown-item>
-                        <b-dropdown-item href="#">FA</b-dropdown-item>
+                    <b-nav-item-dropdown text="Service Links" right>
+                        <b-dropdown-item href="#">Health endpoint</b-dropdown-item>
+                        <b-dropdown-item href="#">Actuator endpoint</b-dropdown-item>
+                        <b-dropdown-item href="#">Swagger UI</b-dropdown-item>
                     </b-nav-item-dropdown>
 
-                    <b-nav-item-dropdown right>
-                        <!-- Using button-content slot -->
-                        <template slot="button-content">
-                            <em>User</em>
-                        </template>
-                        <b-dropdown-item href="#">Profile</b-dropdown-item>
-                        <b-dropdown-item href="#">Signout</b-dropdown-item>
+                    <b-nav-item-dropdown text="Application Links" right>
+                        <b-dropdown-item href="/actuator/prometheus">Service actuator endpoint for Prometheus</b-dropdown-item>
+                        <b-dropdown-item href="http://localhost:9090/targets">Prometheus instance</b-dropdown-item>
+                        <b-dropdown-item href="http://localhost:3000">Grafana instance</b-dropdown-item>
+                        <b-dropdown-item href="http://localhost:9200/">Elasticsearch instance</b-dropdown-item>
+                        <b-dropdown-item href="http://localhost:5601">Kibana instance</b-dropdown-item>
                     </b-nav-item-dropdown>
+
                 </b-navbar-nav>
 
             </b-collapse>
