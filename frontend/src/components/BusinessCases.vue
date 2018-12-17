0be06b212df4c4f101f63145e632563c7e12b1b5
Markus Hanses <me@markushanses.de>
2018-12-17 16:00:16 +0100

    REST call by button click. Not working, yet.

--- frontend/src/components/BusinessCases.vue
+++ frontend/src/components/BusinessCases.vue
@@ -1,12 +1,44 @@
 <template>
-    <div class="BusinessCases">
-        <pre v-text="$attrs"/>
+    <div class="container">
+        <div class="BusinessCases">
+            <div style="margin-top: 2rem; padding-bottom: 1rem;"><h1>Apartment rents</h1></div>
+            <div style="padding: 1rem;">
+                <button  @click="callRestService()" type="button" class="btn btn-primary" style="margin-bottom: 1rem; margin-right: 1rem;">Start a rent</button>
+                <button type="button" class="btn btn-warning" style="margin-bottom: 1rem; margin-right: 1rem;">Abort a rent</button>
+                <button type="button" class="btn btn-success" style="margin-bottom: 1rem; margin-right: 1rem;">Confirm a rent</button>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+    // import axios from 'axios'
+    import {AXIOS} from './http-common'
+
     export default {
-        props: {
+        name: 'business-cases',
+
+        data () {
+            return {
+                msg: 'HowTo call REST-Services:',
+                response: [],
+                errors: []
+            }
         },
-    };
+        methods: {
+            // Fetches posts when the component is created.
+            callRestService () {
+                AXIOS.get(`/hello`)
+                    .then(response => {
+                        // JSON responses are automatically parsed.
+                        this.response = response.data
+                        console.log(response.data)
+                    })
+                    .catch(e => {
+                        this.errors.push(e)
+                    })
+            }
+        }
+    }
+
 </script>
---
+++ frontend/src/components/http-commons.js
@@ -0,0 +1,5 @@
+import axios from 'axios'
+
+export const AXIOS = axios.create({
+    baseURL: `http://localhost:8082`
+})
