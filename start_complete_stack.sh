4c75ea70af3e9de60fda2aa544e8420c721a1a12
Markus Hanses <me@markushanses.de>
2019-01-06 16:57:26 +0100

    Authentication in grafana deactivated. Bash script separated into start and stop scripts.

--- start_complete_stack.sh
+++ start_complete_stack.sh
@@ -1,8 +1,3 @@
 #!/usr/bin/env bash
 
-docker-compose stop && docker-compose rm -f -v
-docker volume remove spring-boot-micrometer-sample_elasticsearch_data
-docker volume remove spring-boot-micrometer-sample_grafana_data
-docker volume remove spring-boot-micrometer-sample_prometheus_data
-docker volume remove spring-boot-micrometer-sample_kibana_data
 docker-compose up -d --build
