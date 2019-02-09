54bec7fa798b6769f3694c4d20da73ef5da96be1
Markus Hanses <hanses.markus@gmail.com>
2019-02-09 18:56:05 +0100

    Service API improved by endpoint to retrieve one specific apartment.

--- spring-boot/src/main/java/de/markushanses/sample/micrometer/sample/adapter/rest/controller/apartment/ApartmentController.java
+++ spring-boot/src/main/java/de/markushanses/sample/micrometer/sample/adapter/rest/controller/apartment/ApartmentController.java
@@ -12,36 +12,48 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @Api(value = "Apartment management endpoint", description = "Operation for apartment lifecycle management")
 @CrossOrigin(origins = "*", allowedHeaders = "*")
-@RequestMapping(path = "api/apartment", produces = MediaType.APPLICATION_JSON_VALUE)
+
 public class ApartmentController {
 
-	private final ApartmentRepository apartmentRepository;
-	private final ApartmentResourceMapper apartmentResourceMapper;
+    private final ApartmentRepository apartmentRepository;
+    private final ApartmentResourceMapper apartmentResourceMapper;
 
-	@Autowired
-	public ApartmentController(ApartmentRepository apartmentRepository, ApartmentResourceMapper apartmentResourceMapper) {
-		this.apartmentRepository = apartmentRepository;
-		this.apartmentResourceMapper = apartmentResourceMapper;
-	}
+    @Autowired
+    public ApartmentController(ApartmentRepository apartmentRepository, ApartmentResourceMapper apartmentResourceMapper) {
+        this.apartmentRepository = apartmentRepository;
+        this.apartmentResourceMapper = apartmentResourceMapper;
+    }
 
-	@ApiOperation(value = "View a list of available employees")
-	@ApiResponses(value = {
-			@ApiResponse(code = 200, message = "Successfully retrieved list")
-	})
-	@GetMapping
-	public ResponseEntity<List<ApartmentResource>> retrieveAll() {
-		List<Apartment> apartmentList = apartmentRepository.findAll();
-		List<ApartmentResource> apartmentResourceList = apartmentResourceMapper.mapApartmentsToResources(apartmentList);
-		return new ResponseEntity<>(apartmentResourceList, HttpStatus.OK);
-	}
+    @ApiOperation(value = "View a list of available apartments")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list")
+    })
+    @GetMapping(path = "api/apartment", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<ApartmentResource>> retrieveAll() {
+        List<Apartment> apartmentList = apartmentRepository.findAll();
+        List<ApartmentResource> apartmentResourceList = apartmentResourceMapper.mapApartmentsToResources(apartmentList);
+        return new ResponseEntity<>(apartmentResourceList, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "View a the specified apartment")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Successfully retrieved list")
+    })
+    @GetMapping(path = "api/apartment/{apartmentId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ApartmentResource> retrieve(@PathVariable(name = "apartmentId") UUID apartmentId) {
+        Apartment apartment = apartmentRepository.findByApartmentId(apartmentId);
+        ApartmentResource apartmentResource = apartmentResourceMapper.mapApartmentToResource(apartment);
+        return new ResponseEntity<>(apartmentResource, HttpStatus.OK);
+    }
 
 
 }
