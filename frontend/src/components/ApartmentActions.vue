6955391fda358279c0a8627d8b5b848d6381f202
Markus Hanses <hanses.markus@gmail.com>
2019-02-12 21:22:31 +0100

    User feedback improved.

--- frontend/src/components/ApartmentActions.vue
+++ frontend/src/components/ApartmentActions.vue
@@ -1,8 +1,23 @@
 <template>
     <b-container>
         <div style="margin-top: 2rem; padding-bottom: 1rem;"><h1>Apartment</h1></div>
-
         <div>
+            <b-row>
+                <b-col>
+                    <b-alert :show="serviceErrors.length <= 0 && apartmentRentSuccess !== null" variant="success">
+                        <h4>Booking action successful</h4>
+                        <hr>
+                        <p class="mb-0">{{ apartmentRentSuccess }}</p>
+                    </b-alert>
+                    <b-alert :show="serviceErrors.length > 0"
+                             v-for="error in serviceErrors" :key="error"
+                             variant="danger">
+                        <h4>Could not update apartment!</h4>
+                        <hr>
+                        <p class="mb-0">{{ error }}</p>
+                    </b-alert>
+                </b-col>
+            </b-row>
             <b-form @submit="onSubmit" @reset="onReset" v-if="show">
                 <b-form-group id="apartmentIdInputGroup"
                               label="Id:"
@@ -90,7 +105,8 @@
                     {text: 'Cancel rent', value: 'free', disabled: false},
                 ],
                 selected: '',
-                show: true
+                show: true,
+                apartmentRentSuccess: null
             }
         },
         mounted: function () {
@@ -146,42 +162,45 @@
                 this.selected = '';
             },
             callReserveApartmentService(apartmentId) {
-                this.apartmentActionName = "Reserve apartment";
-                this.apartmentActionErrors = [];
+                this.serviceErrors = [];
 
                 Axios.post("/apartment-rent/reserve", {"apartmentId": apartmentId})
                     .then(response => {
-                        this.apartmentActionSuccessResponse = response.data;
+                        if (response.status == 201) {
+                            this.apartmentRentSuccess = "Apartment reserved.";
+                        }
                     })
                     .catch(error => {
-                        this.apartmentActionSuccessResponse = null;
-                        this.apartmentActionErrors.push(error.message);
+                        this.apartmentRentSuccess = null;
+                        this.serviceErrors.push(error.message);
                     })
             },
             callConfirmApartmentRentService(apartmentId) {
-                this.apartmentActionName = "Confirm apartment rent";
-                this.apartmentActionErrors = [];
+                this.serviceErrors = [];
 
                 Axios.post("/apartment-rent/confirm", {"apartmentId": apartmentId})
                     .then(response => {
-                        this.apartmentActionSuccessResponse = response.data;
+                        if (response.status == 201) {
+                            this.apartmentRentSuccess = "Apartment rent started.";
+                        }
                     })
                     .catch(error => {
-                        this.apartmentActionSuccessResponse = null;
-                        this.apartmentActionErrors.push(error.message);
+                        this.apartmentRentSuccess = null;
+                        this.serviceErrors.push(error.message);
                     })
             },
             callCancelApartmentRentService(apartmentId) {
-                this.apartmentActionName = "Cancel apartment rent";
-                this.apartmentActionErrors = [];
+                this.serviceErrors = [];
 
                 Axios.post("/apartment-rent/cancel", {"apartmentId": apartmentId})
                     .then(response => {
-                        this.apartmentActionSuccessResponse = response.data;
+                        if (response.status == 201) {
+                            this.apartmentRentSuccess = "Apartment rent cancelled.";
+                        }
                     })
                     .catch(error => {
-                        this.apartmentActionSuccessResponse = null;
-                        this.apartmentActionErrors.push(error.message);
+                        this.apartmentRentSuccess = null;
+                        this.serviceErrors.push(error.message);
                     })
             }
         }
