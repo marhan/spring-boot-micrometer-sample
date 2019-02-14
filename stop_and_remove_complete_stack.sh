3c0ea027151dbdf8120781f0bc15b0fa595d135f
Markus Hanses <markus.hanses@immonet.de>
2019-02-14 12:59:37 +0100

    Chaos monkey default configuration adjusted.

--- stop_and_remove_complete_stack.sh
+++ stop_and_remove_complete_stack.sh
@@ -4,4 +4,3 @@ docker-compose stop && docker-compose rm -f -v
 docker volume remove spring-boot-micrometer-sample_elasticsearch_data
 docker volume remove spring-boot-micrometer-sample_grafana_data
 docker volume remove spring-boot-micrometer-sample_prometheus_data
-docker volume remove spring-boot-micrometer-sample_kibana_data
